ba6c38630a83b31595e96a23258884c57213946f
Cova97 <acovamartinez@gmail.com>
2023-10-20 14:08:25 -0600

    vgg19
    red neuronal vgg19

--- detection.py
+++ detection.py
@@ -1,13 +1,23 @@
 import cv2
 import numpy as np
 import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
 
 # Cargar el modelo entrenado
 model = tf.keras.models.load_model('CNN_Modelo.h5')
 
-# Abrir el archivo .mp4 
+# Abrir el archivo .mp4
 video_path = 'Arma.mp4'
 cap = cv2.VideoCapture(video_path)
+
+# Crear el generador de datos para normalización
+data_gen = ImageDataGenerator(
+    rotation_range=90,  # Rango de rotación aleatoria de hasta 90 grados
+    horizontal_flip=True  # Reflejo horizontal aleatorio
+)
+
+# Inicializar la ventana para mostrar el video
+cv2.namedWindow('Video', cv2.WINDOW_NORMAL)
 
 # Procesar el video fotograma por fotograma
 while cap.isOpened():
@@ -15,11 +25,24 @@ while cap.isOpened():
     if not ret:
         break
 
-    # Realizar la inferencia con el modelo
-    input_data = np.expand_dims(frame, axis=0)  # Agregar una dimensión adicional para el lote
-    predictions = model.predict(input_data)
+    # Redimensionar el fotograma a 150x150 píxeles
+    frame = cv2.resize(frame, (150, 150))
 
-    cv2.imshow('Video', frame)
+    # Normalizar la imagen
+    frame = data_gen.standardize(np.array([frame]))
+
+    # Realizar la inferencia con el modelo
+    predictions = model.predict(frame)
+
+    # Interpretar las predicciones
+    class_index = np.argmax(predictions)
+    class_labels = ['Weapon', 'Knife']
+    class_label = f"Clase predicha: {class_labels[class_index]}"
+
+    # Dibujar el cuadro de texto en el fotograma
+    cv2.putText(frame[0], class_label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 255), 2)
+
+    cv2.imshow('Video', frame[0])  # Mostrar el fotograma
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
--- trainvgg16.py
+++ trainvgg16.py
@@ -1,12 +1,15 @@
 import tensorflow as tf
 from tensorflow import keras
 from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import GlobalAveragePooling2D, Dense
+from keras.models import Model
 
 
 # Rutas de las carpetas test, train y valid
 train_dir = 'train' 
 valid_dir = 'valid'
 test_dir = 'test'
+BATCH_SIZE = 32
 
 # Creacion de las clases 
 class_names = ['weapon', 'knife']
@@ -29,49 +32,59 @@ test_data_gen = ImageDataGenerator(
 train_generator = train_data_gen.flow_from_directory(
     train_dir,
     target_size=(150, 150),
-    batch_size=32,
-    class_mode='sparse'
+    batch_size=BATCH_SIZE,
+    color_mode="rgb",
+    class_mode='categorical', shuffle=True
 )
 
 valid_generator = valid_data_gen.flow_from_directory(
     valid_dir,
     target_size=(150, 150),
-    batch_size=32,
-    class_mode='sparse'
+    batch_size=BATCH_SIZE,
+    color_mode="rgb",
+    class_mode='categorical', shuffle=True
 )
 
 test_generator = test_data_gen.flow_from_directory(
     test_dir,
     target_size=(150, 150),
-    batch_size=32,
-    class_mode='sparse'
+    batch_size=BATCH_SIZE,
+    color_mode="rgb",
+    class_mode='categorical', shuffle=True
 )
 
-# Arquitectura de la CNN
-model = keras.Sequential([
-    keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)),
-    keras.layers.MaxPooling2D((2, 2)),
-    keras.layers.Conv2D(64, (3, 3), activation='relu'),
-    keras.layers.MaxPooling2D((2, 2)),
-    keras.layers.Conv2D(128, (3, 3), activation='relu'),
-    keras.layers.MaxPooling2D((2, 2)),
-    keras.layers.Flatten(),
-    keras.layers.Dense(512, activation='relu'),
-    keras.layers.Dropout(0.5),
-    keras.layers.Dense(len(class_names), activation='softmax')
-])
+vgg = tf.keras.applications.VGG16(input_shape=(150,150,3),include_top=False,weights="imagenet")
+
+vgg.summary()
+
+def build_model(bottom_model, classes):
+    model = bottom_model.layers[-2].output
+    model = GlobalAveragePooling2D()(model)
+    model = Dense(classes, activation='softmax', name="Capa_Salida")(model)
+
+    return model
+
+head = build_model(vgg, 2)
+
+model = Model(inputs= vgg.input, outputs = head)
+model.summary()
+
 
 model.compile(
     optimizer='adam',
-    loss='sparse_categorical_crossentropy',
+    loss='categorical_crossentropy',
     metrics=['accuracy']
 )
 
 # Entrenar el modelo con los generadores
-history_model = model.fit(train_generator, validation_data=valid_generator, epochs=100)
+history_model = model.fit(train_generator, 
+                          validation_data=valid_generator, 
+                          epochs=50, 
+                          steps_per_epoch=train_generator.n//BATCH_SIZE,
+                          validation_steps=valid_generator.n//BATCH_SIZE)
 
 # Guardar el modelo
-model.save('CNN_Modelo.h5')
+model.save('CNN_Modelo-VGG16.h5')
 
 test_loss, test_accuracy = model.evaluate(test_generator)
 print(f'Accuracy: {test_accuracy}')
